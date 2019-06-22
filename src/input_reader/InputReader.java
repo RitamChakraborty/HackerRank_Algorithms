a56501da9fa96e7500c4db78c5ae9f2d88edcd12
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-06-22 12:12:12 +0530

    Added singleton class of InputReader to read input from testcases

---
+++ src/input_reader/InputReader.java
@@ -0,0 +1,25 @@
+package input_reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputReader {
+    public static String read(String path) {
+        int i = 0;
+        BufferedReader bufferedReader;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(path));
+            while ((i = bufferedReader.read()) != -1) {
+                stringBuilder.append((char) i);
+            }
+
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
