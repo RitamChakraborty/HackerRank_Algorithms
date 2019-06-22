2ab6e8218a7afe9d3990de03baec525283a15b08
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-06-22 16:07:06 +0530

    Solved 2D Array problem

--- src/README.md
+++ src/README.md
@@ -4,3 +4,4 @@ Hacker Rank problems solved in java. You can find all the problems in [Hacker-Ra
 
 ### Problems
  - [Jumping on the Clouds](https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem)
+ - [2D Array](https://www.hackerrank.com/challenges/2d-array/problem)
---
+++ src/twod_array/Main.java
@@ -0,0 +1,77 @@
+// https://www.hackerrank.com/challenges/2d-array/problem
+
+package twod_array;
+
+import input_reader.InputReader;
+
+import java.util.*;
+
+public class Main {
+
+    private static int singleHourglassSum(Integer[][] arr) {
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1) {
+                    if (j == 1) {
+                        sum += arr[i][j];
+                    }
+                } else {
+                    sum += arr[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private static int hourglassSum(int[][] arr) {
+        int sum = 0;
+        Integer[][][][] hourglassList = new Integer[4][4][3][3];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        hourglassList[i][j][k][l] = arr[i + k][j + l];
+                    }
+                }
+
+                list.add(singleHourglassSum(hourglassList[i][j]));
+            }
+        }
+
+        return Collections.max(list);
+    }
+
+    public static void main(String[] args) {
+        String path = "/media/ritam/Storage/Projects/IdeaProjects/Playground/Hacker Rank/src/twod_array/testcase.txt";
+        String input = InputReader.read(path);
+
+        int[][] arr = new int[6][6];
+        String[] lines = input.split("\n");
+
+        for (int i = 0; i < 6; i++) {
+            String[] nums = lines[i].split(" ");
+            ArrayList<String> list = new ArrayList<>();
+
+            for (String num: nums) {
+                String[] s = num.split(" ");
+
+                for (String s1: s) {
+                    if (!s1.equals(" ") && s1.length() != 0) {
+                        list.add(s1);
+                    }
+                }
+            }
+
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = Integer.parseInt(list.get(j));
+            }
+        }
+
+        System.out.println(hourglassSum(arr));
+    }
+}
