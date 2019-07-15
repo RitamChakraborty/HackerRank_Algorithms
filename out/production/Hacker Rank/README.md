4b65e77bb79b385dbe0f0dfd1076f4472a025de8
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-07-15 22:16:07 +0530

    Reduce the time complexity of Array Manipulation problem using prefix sum array algorithm

--- out/production/Hacker Rank/README.md
+++ out/production/Hacker Rank/README.md
@@ -8,3 +8,4 @@ Hacker Rank problems solved in java. You can find all the problems in [Hacker-Ra
  - [Arrays: Left Rotation](https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem)
  - [Left Rotation](https://www.hackerrank.com/challenges/array-left-rotation/problem)
  - [Sparse Arrays](https://www.hackerrank.com/challenges/sparse-arrays/problem)
+ - [Minimum Distances](https://www.hackerrank.com/challenges/minimum-distances/problem)
--- src/array_manipulation/Main.java
+++ src/array_manipulation/Main.java
@@ -1,30 +1,35 @@
 // https://www.hackerrank.com/challenges/crush/problem
-// Todo: Reduce time complexity
 
 package array_manipulation;
 
 import input_reader.InputReader;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
 
     private static long arrayManipulation(int n, int[][] queries) {
-        Long[] arr = new Long[n];
-        Arrays.fill(arr, 0L);
+        long[] arr = new long[n + 1];
 
-        for (int[] query : queries) {
-            int a = query[0] - 1;
-            int b = query[1] - 1;
-            int k = query[2];
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0] - 1;
+            int b = queries[i][1];
+            int k = queries[i][2];
 
-            for (int j = a; j <= b; j++) {
-                arr[j] += k;
-            }
+            arr[a] += k;
+            arr[b] -= k;
         }
 
-        return Collections.max(Arrays.asList(arr));
+        System.out.println(Arrays.toString(arr));
+
+        long max = Long.MIN_VALUE;
+        long sum = 0;
+        for (int i = 0; i < n + 1; i++) {
+            sum += arr[i];
+            max = Long.max(sum, max);
+        }
+
+        return max;
     }
 
     public static void main(String[] args) {
