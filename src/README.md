22b5c1d303f575e2ecc6c100bff657a57b2477c3
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-09-03 12:42:30 +0530

    Solve Picking Numbers

--- src/README.md
+++ src/README.md
@@ -18,4 +18,5 @@ Hacker Rank problems solved in java. You can find all the problems in [Hacker-Ra
  - [String: Comparator](https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem)
  - [New Year Chaos](https://www.hackerrank.com/challenges/new-year-chaos/problem)
  - [Sherlock and Valid String](https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem)
+ - [Picking Numbers](https://www.hackerrank.com/challenges/picking-numbers/problem)
  
---
+++ src/picking_numbers/Main.java
@@ -0,0 +1,48 @@
+// https://www.hackerrank.com/challenges/picking-numbers/problem
+
+package picking_numbers;
+
+import input_reader.InputReader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+	public static int pickingNumbers(List<Integer> list) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i : list) {
+			map.put(i, map.getOrDefault(i, 0) + 1);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for (int i : map.keySet()) {
+			if (map.keySet().contains(i + 1)) {
+				int n = map.get(i) + map.get(i + 1);
+				if (n > max) {
+					max = n;
+				}
+			} else {
+				if (map.get(i) > max) {
+					max = map.get(i);
+				}
+			}
+		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		String path = "/media/ritam/Storage/Projects/IdeaProjects/Playground/Hacker Rank/src/picking_numbers/testcase.txt";
+		String input = InputReader.read(path);
+		String[] inputs = input.split("\n");
+		
+		int n = Integer.parseInt(inputs[0]);
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(inputs[1].split(" ")[i]));
+		}
+		
+		System.out.println(pickingNumbers(list));
+	}
+}
