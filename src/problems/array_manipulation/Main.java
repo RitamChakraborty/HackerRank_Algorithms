907789dc290cba3ada47d6d409b29fef99e113a6
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-02-02 00:43:00 +0530

    Remove Jumping on the Clouds

--- src/problems/array_manipulation/Main.java
+++ src/problems/array_manipulation/Main.java
@@ -10,18 +10,18 @@ public class Main {
 
     private static long arrayManipulation(int n, int[][] queries) {
         long[] arr = new long[n + 1];
-
-        for (int i = 0; i < queries.length; i++) {
-            int a = queries[i][0] - 1;
-            int b = queries[i][1];
-            int k = queries[i][2];
-
+    
+        for (int[] query : queries) {
+            int a = query[0] - 1;
+            int b = query[1];
+            int k = query[2];
+        
             arr[a] += k;
             arr[b] -= k;
         }
-
+    
         System.out.println(Arrays.toString(arr));
-
+    
         long max = Long.MIN_VALUE;
         long sum = 0;
         for (int i = 0; i < n + 1; i++) {
